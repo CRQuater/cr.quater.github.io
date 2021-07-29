d375c15d8b7bcaf124c6ad4fa1acb09a69e2765f
CRQuater <rhquater@gmail.com>
2021-07-29 17:39:31 +0700

    Basic
    Add some more linear stuff

--- myScript.js
+++ myScript.js
@@ -6,3 +6,18 @@ function myFunction(){
         x.style.display = "none";
     }
 }
+function clickDrop(){
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
